e3ecc48eb601e0b026db7449bc785c554f27a275
zaystore242 <yudaaguz099@gmail.com>
2025-11-14 16:26:19 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,10 +1,11 @@
-const domain = "https://panel.adhamproject.my.id"
-const apikey = "ptla_Lio6nppuz8UqLfTrbj5diHguqbwJe9k8fs1YIPrYXma"
-const atlakey = ""
+const domain = "https://syahadpprivate.anti-ddos.me"
+const apikey = "ptla_KqduGtbk1NwOKysQHplpIqyBRvMFkJP5QGzYHo2tRID"
+const atlakey = "nkdMlaz0tcR6kh6Jvu6LIAnC8B7HcPWplQKegULkwbcAxogz3oL4jMe4A4e4YyA1tZ0JpFwcBH6EnGGJJKS1p5BH8NSaVVGeGFmv"
 
 module.exports = { 
     domain, apikey, atlakey
 
 }
+
 
 
